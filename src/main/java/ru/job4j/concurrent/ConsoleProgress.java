2dc4f07f08d55fd4f6e98fcc6215b0e8755eea7a
corsair <corsair954@mail.ru>
2026-01-10 15:44:02 +0300

    6. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -5,14 +5,15 @@ public class ConsoleProgress implements Runnable {
     public void run() {
         var index = 4;
         var process = new char[] {'-', '\\', '|', '/'};
-        try {
             while (!Thread.currentThread().isInterrupted()) {
-                System.out.print("\rLoading ..." + process[index++ % 4]);
-                Thread.sleep(500);
+                try {
+                    System.out.print("\rLoading ..." + process[index++ % 4]);
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    System.out.println("\r\nПроцесс завершен");
+                }
             }
-        } catch (InterruptedException e) {
-            System.out.println("\r\nПроцесс завершен");
-        }
     }
 
     public static void main(String[] args) throws InterruptedException {
