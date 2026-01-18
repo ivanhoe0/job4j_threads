3306f60c6237a6b02de30b19728cfdf4c2166d3b
corsair <corsair954@mail.ru>
2026-01-18 16:33:57 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,36 @@
+package ru.job4j.buffer;
+
+import ru.job4j.producerconsumer.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(4);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 6; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+
+        ).start();
+    }
+}
