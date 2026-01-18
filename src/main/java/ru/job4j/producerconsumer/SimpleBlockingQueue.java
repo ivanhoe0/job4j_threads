eccecb13c76ed5f5c6e9271ac3040fc3b4308078
corsair <corsair954@mail.ru>
2026-01-18 15:18:23 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/producerconsumer/SimpleBlockingQueue.java
@@ -0,0 +1,51 @@
+package ru.job4j.producerconsumer;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    private final int size;
+
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
+
+    public SimpleBlockingQueue(int size) {
+        this.size = size;
+    }
+
+    public void offer(T value) {
+        synchronized (this) {
+            while (size == queue.size()) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            if (queue.size() == 0) {
+                this.notifyAll();
+            }
+            queue.offer(value);
+        }
+    }
+
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public T poll() throws InterruptedException {
+        synchronized (this) {
+            while (queue.size() == 0) {
+                this.wait();
+            }
+            if (size == queue.size()) {
+                this.notifyAll();
+            }
+            return queue.poll();
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/producerconsumer/SimpleBlockingQueueTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.producerconsumer;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleBlockingQueueTest {
+    @Test
+    void whenAddAndGet() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(4);
+        List<Integer> buffer = new ArrayList<>();
+        Thread producer = new Thread(() -> {
+            IntStream.range(0, 5).forEach(queue::offer);
+        });
+        producer.start();
+        Thread consumer = new Thread(() -> {
+           while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+               try {
+                   buffer.add(queue.poll());
+               } catch (InterruptedException e) {
+                   Thread.currentThread().interrupt();
+               }
+           }
+        });
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).containsExactly(0, 1, 2, 3, 4);
+    }
+}
