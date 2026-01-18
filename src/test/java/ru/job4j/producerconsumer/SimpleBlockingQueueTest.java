33f16755f6000d8caf354b40b58d476225424437
corsair <corsair954@mail.ru>
2026-01-18 17:29:02 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/producerconsumer/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/producerconsumer/SimpleBlockingQueueTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,5 +32,29 @@ class SimpleBlockingQueueTest {
         consumer.interrupt();
         consumer.join();
         assertThat(buffer).containsExactly(0, 1, 2, 3, 4);
+    }
+
+    @Test
+    void whenSizeLess() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        Thread producer = new Thread(() -> {
+           IntStream.range(0, 10).forEach(queue::offer);
+        });
+        producer.start();
+        Thread consumer = new Thread(() -> {
+            while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                try {
+                    buffer.add(queue.poll());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
     }
 }
